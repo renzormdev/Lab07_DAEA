afd7ad12e5aaf06d2919fa52408792c679ec8d77
Alumno <Alumno@LAB1507-09>
2024-10-01 21:23:55 -0500

    subida

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo07
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private InvoiceBusiness invoiceBusiness = new InvoiceBusiness();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListarButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadInvoices();
+        }
+
+        private void LoadInvoices()
+        {
+            InvoicesDataGrid.ItemsSource = invoiceBusiness.GetAllInvoices();
+        }
+    }
+}
